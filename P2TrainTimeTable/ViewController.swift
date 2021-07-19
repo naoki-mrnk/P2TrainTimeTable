8b09d8d8171833f3405778ace0d68198a043dfbf
naoki-mrnk <20cm0135@jec.ac.jp>
2021-07-19 15:15:18 +0900

    Initial Commit

---
+++ P2TrainTimeTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P2TrainTimeTable
+//
+//  Created by cmStudent on 2021/07/19.
+//  Copyright © 2021 naoki-mrnk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
