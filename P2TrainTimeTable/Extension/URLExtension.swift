da5566601cdeabc7ae557f24b994ef2a1fad679f
naoki-mrnk <20cm0135@jec.ac.jp>
2021-07-22 18:39:56 +0900

    URLExtensionの作成、不要なファイルの削除

--- P2TrainTimeTable/Extension/URLExtension.swift
+++ P2TrainTimeTable/Extension/URLExtension.swift
@@ -1,9 +1,13 @@
 //
-//  UIButtonExtension.swift
+//  URLExtension.swift
 //  P2TrainTimeTable
 //
-//  Created by naoki-mrnk on 2021/07/19.
+//  Created by naoki-mrnk on 2021/07/22.
 //  Copyright © 2021 naoki-mrnk. All rights reserved.
 //
 
 import Foundation
+
+extension URL {
+    
+}
